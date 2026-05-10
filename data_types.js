e3509c20c3dd80e0f2fa001088bc45fd34abe7f2
navneet0217 <navneetanand17360@gmail.com>
2026-05-10 10:51:33 +0530

    data_types

---
+++ data_types.js
@@ -0,0 +1,22 @@
+"use strict"; //treat it as modern javascript
+
+// primitive data types
+//Number --> 2^53 - 1
+//BigInt --> used for large integers
+//String --> ""
+//Boolean --> true or false
+//null --> intentional absence of value/standalone value
+//undefined --> value is not assigned
+//Symbol --> used for unique values
+
+// non-primitive data types
+//Array
+//Object
+//Function
+
+// alert(2+4);  //browser specific not in nodejs
+
+//code readibility should be high
+
+console.log(typeof null); //object (bug in JS)
+console.log(typeof undefined); //undefined
--- index.js
+++ index.js
@@ -1,1 +1,2 @@
 console.log("Lets start Javascript");
+
