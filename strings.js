3f248de35614846edc543fb58a99fce6810f0388
navneet0217 <navneetanand17360@gmail.com>
2026-05-11 12:42:55 +0530

    Strings and its functions

---
+++ strings.js
@@ -0,0 +1,27 @@
+// const name = "Navneet";
+// const billAmount = 80;
+
+// // console.log(name + amount);
+
+// //the correct way to do this is using string interpolation
+
+// console.log(`Hello ${name} and your bill is ${billAmount}`);
+
+const myString = new String("Navneet");
+console.log(myString);
+
+//string functions
+// console.log(myString.charAt(6));
+// console.log(myString.at(0));
+// console.log(myString.length);
+// console.log(myString.slice(4))
+// console.log(myString.substring(0, 4))
+// console.log(myString.split(""));
+// console.log(myString.trim());
+// console.log(myString.replace('Navneet', "Navneet-Kumar"));
+// console.log(myString.toLowerCase());
+// console.log(myString.toUpperCase());
+// console.log(myString.search('a'))
+
+//There are many String functions it will be used acc to usecase , just go to MDN Docs
+
