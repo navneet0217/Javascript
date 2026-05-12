dfe606b676372b893875affc04547a13f20074cb
navneet0217 <navneetanand17360@gmail.com>
2026-05-12 18:53:39 +0530

    Arrays-1

---
+++ 02_basics/Arrays.js
@@ -0,0 +1,37 @@
+//Arrays
+
+const arr = [1, 2, 3, 4, 5, 6];
+// console.log(arr[0]);
+// const cities = ["Jaipur", "Udaipur", "Sikar", "Jodhpur"];
+// console.log(cities[3]);
+
+// const myHundreds = new Array(100, 200, 300, 400);
+// console.log(myHundreds[0]);
+
+//Arrays methods
+
+// myHundreds.push(500);
+// console.log(myHundreds); //push element at end
+// myHundreds.pop();
+// console.log(myHundreds); //pop element from end
+
+// arr.unshift(9);
+// console.log(arr); // unshift element at start
+// arr.shift();          //shift element from start
+// console.log()
+
+// console.log(arr.includes(2));
+// console.log(arr.indexOf(2));
+
+// const myArr = arr.join(); //converts array to string
+// console.log(typeof myArr);
+
+//slice and spice
+// console.log("Before slice op: ");
+// console.log(arr); //it includes the element at index 1 but does not include the element at index 3
+// console.log("After slice op: ", arr.slice(1, 3));
+
+// console.log("Array after slice", arr);
+
+// console.log("After splice op:", arr.splice(1, 3)); // it includes the element at index 1 and it includes the element at index 3 and also changes the original array 
+// console.log("Array After splice", arr);
